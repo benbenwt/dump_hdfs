800cc6ed3cc58c1b8eabc363ca815d6bcccc4193
benbenwt <2407117028@qq.com>
2021-05-21 14:26:04 +0800

    nlp,cve

--- src/main/java/com/ti/dump_hdfs/Insert_stix.java
+++ src/main/java/com/ti/dump_hdfs/Insert_stix.java
@@ -1,52 +1,95 @@
 package com.ti.dump_hdfs;
 
+
 import com.ti.dump_hdfs.util.ProduceHdfs;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.io.*;
 import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
+
 
 public class Insert_stix {
-    private  ProduceHdfs produceHdfs;
+    private  ProduceHdfs produceHdfs=new ProduceHdfs();
 
     public  void dump_hdfs(String hdfsPath,String localPath) throws IOException {
+        localPath=localPath.replace("\"","");
         Configuration configuration = new Configuration();
         configuration.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
         configuration.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-        FileSystem fs = FileSystem.get(URI.create(hdfsPath), configuration);
-        FSDataOutputStream out = fs.create(new Path(hdfsPath));//创建一个输出流
-        InputStream in = new FileInputStream(new File(localPath));//从本地读取文件
+
+        String filename= UUID.randomUUID().toString();
+
+        System.out.println("hdfspath: "+hdfsPath+filename+".json");
+        System.out.println(" localpath : "+localPath);
+        FileSystem fs = FileSystem.get(URI.create(hdfsPath+ filename+".json"), configuration);
+        FSDataOutputStream out = fs.create(new Path(hdfsPath+filename+".json"));//创建一个输出流
+        InputStream in = null;
+        try{
+             in = new FileInputStream(new File(localPath));//从本地读取文件
+        }catch (Exception e)
+        {
+            System.out.println(e);
+            return;
+        }
+
         IOUtils.copyBytes(in, out, 100, true);
+        produceHdfs.produceHdfsPath(hdfsPath+filename+".json");
     }
 
+    public static String getCurrentDate()
+    {
+        Date date=new Date();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        String format_date=simpleDateFormat.format(date);
+        return  format_date;
+    }
+
+    public  void dump_hdfsByStringContent(String content) throws IOException {
+        String hdfsPath="hdfs://hbase:9000/user/root/stix/"+getCurrentDate()+"fromNlptest1/";
+        String filename= UUID.randomUUID().toString();
+        String filepath=hdfsPath+filename+".json";
+        Configuration configuration = new Configuration();
+        configuration.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+        configuration.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+//        upload content to  hdfs
+        InputStream in=new ByteArrayInputStream(content.getBytes("UTF-8"));
+        FileSystem fs=FileSystem.get(URI.create(filepath),configuration);
+        FSDataOutputStream out=fs.create(new Path(filepath));
+        System.out.println(filepath);
+        IOUtils.copyBytes(in,out,100,true);
+        produceHdfs.produceHdfsPath(filepath);
+    }
     public  void  submitStixDirectory(String stix_directory,String hdfs_directory) throws IOException {
         produceHdfs=new ProduceHdfs();
         File file=new File(stix_directory.replace("\"",""));
-        String new_hdfs_directory=hdfs_directory+getCurrentTime()+"/";
+        String name = "";
+        String hdfsPath = "";
         if(file.exists())
         {
             File[] fs=file.listFiles();
             for(File f:fs)
             {
-                String name=f.getName();
+                name=f.getName();
+                hdfsPath=hdfs_directory+name;
                 if(!f.isDirectory()&&name.endsWith("json"))
                 {
-                    dump_hdfs(new_hdfs_directory+name,f.getPath());
+                    dump_hdfs(hdfsPath,f.getPath());
                 }
             }
             System.out.println(stix_directory+",上传完毕");
             if(fs.length>0)
             {
-                produceHdfs.produceHdfsPath(new_hdfs_directory);
+                produceHdfs.produceHdfsPath(hdfsPath);
             }
         }
     }
@@ -59,6 +102,6 @@ public class Insert_stix {
     }
 
     public static void main(String[] args) throws IOException {
-        new Insert_stix().submitStixDirectory(args[0],args[1]);
+        new Insert_stix().dump_hdfsByStringContent("aaaaaaatest");
     }
 }
--- src/main/java/com/ti/dump_hdfs/util/ConsumeLisa.java
+++ src/main/java/com/ti/dump_hdfs/util/ConsumeLisa.java
@@ -2,6 +2,7 @@ package com.ti.dump_hdfs.util;
 
 
 import com.ti.dump_hdfs.Insert_stix;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -24,7 +25,7 @@ public class ConsumeLisa {
     public  void consume(String url,String topic) throws IOException {
         Properties props = new Properties();
         props.put("bootstrap.servers", url);
-        props.put("group.id", "test");
+        props.put("group.id", "hadoop");
         props.put("enable.auto.commit", "false");
         props.put("auto.commit.interval.ms", "1000");
         props.put("auto.offset.reset", "latest");
@@ -41,13 +42,51 @@ public class ConsumeLisa {
             for (ConsumerRecord<String, String> record : records)
             {
                 System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
-                insert_stix.submitStixDirectory(record.value(),"hdfs://hbase:9000/user/root/stix/"+getCurrentDate()+"/");
+                insert_stix.dump_hdfs("hdfs://hbase:9000/user/root/stix/"+getCurrentDate()+"fromlisa/",record.value());
+            }
+        }
+        consumer.commitSync();
+        consumer.close();
+    }
+
+    public  void consumeStringContent(String url,String topic) throws IOException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", url);
+        props.put("group.id", "dump_hdfs4");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "latest");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("deserializer.encoding","utf-8");
+        props.put("key.deserializer.encoding","utf-8");
+        props.put("value.deserializer.encoding","utf-8");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        consumer.subscribe(Arrays.asList(topic));
+        Insert_stix insert_stix=new Insert_stix();
+        boolean flag = true;
+        String cutString="";
+        while (flag) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records)
+            {
+                System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+                System.out.println();
+//                头部和尾部多余得引号
+                cutString=record.value().replace("\"{","{").replace("}\"","}");
+                cutString= StringEscapeUtils.unescapeJava(cutString);
+                System.out.println("cutString"+cutString);
+                insert_stix.dump_hdfsByStringContent(cutString);
             }
         }
         consumer.close();
     }
 
     public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
-        new ConsumeLisa().consume("hbase:9092","lisa");
+        new ConsumeLisa().consumeStringContent("hbase:9092","nlptohdfs");
+//        new ConsumeLisa().consume("hbase:9092","cvetohdfs");
+//        new ConsumeLisa().consume("hbase:9092","lisatohdfs");
     }
 }
